1c239251bc984d6f748774e882bb203ae2efdba6
ZUV68954 <ZUV68954@educastur.es>
2024-11-25 17:48:45 +0100

    Ejercicio 1 completado.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
 fun main() {
-    println("Hello World!")
+    múltiplos()
+}
+
+fun múltiplos() {
+    for (i in 1..100) {
+        if ((i % 3) == 0 && (i % 5) == 0) {
+            println("triqui")
+        } else if ((i % 3) == 0) {
+            println("tri")
+        } else if ((i % 5) == 0) {
+            println("qui")
+        } else {
+            println(i)
+        }
+    }
 }
