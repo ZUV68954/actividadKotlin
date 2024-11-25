2ecb5ebf4a38f196bceb7ebcc320da3be5a812ef
ZUV68954 <ZUV68954@educastur.es>
2024-11-25 18:00:22 +0100

    Ejercicio 2 completado.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,23 @@
 fun main() {
-    println("Hello World!")
+    println("Introduce dos palabras para comprobar si son un anagrama:")
+    val palabra1 = invertirPalabra(introducirPalabra())
+    val palabra2 = introducirPalabra()
+    if (compararPalabras(palabra1, palabra2)) {
+        println("Las palabras son un anagrama.")
+    } else {
+        println("Las palabras no son un anagrama.")
+    }
+}
+
+fun compararPalabras(palabra1: String, palabra2: String): Boolean {
+    return palabra1 == palabra2
+}
+
+fun introducirPalabra(): String {
+    println("Introduce una palabra:")
+    return readln().lowercase()
+}
+
+fun invertirPalabra(palabra: String): String {
+    return palabra.reversed()
 }
