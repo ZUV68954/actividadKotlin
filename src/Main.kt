ecc40abac9b2ee5b165c064bfb5bce18a4bfe67d
ZUV68954 <ZUV68954@educastur.es>
2024-11-26 19:53:11 +0100

    Ejercicio 4 subido y completado al 99%.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,70 @@
+import kotlin.system.exitProcess
+
+var pista: String = "_|_|_"
 fun main() {
-    println("Hello World!")
+    val acciones: Array<String> = arrayOf("correr", "saltar", "correr", "saltar", "correr", "saltar", "correr")
+    if (pistaCorrecta(pista)) {
+        if (accionesCorrectas(acciones)) {
+            if (carrera(acciones)) {
+                println("$pista true")
+            } else {
+                println("$pista false")
+            }
+        } else {
+            println("Las acciones son ilegales.")
+            exitProcess(-1)
+        }
+    } else {
+        println("La pista no es correcta.")
+        exitProcess(-1)
+    }
+}
+
+fun carrera(acciones: Array<String>): Boolean {
+    val obstáculos = pista.toCharArray()
+    var correcto = true
+    for (i in acciones.indices) {
+        try {
+            if (acciones[i] == "correr" && obstáculos[i] != '_') {
+                obstáculos[i] = '/'
+                correcto = false
+            } else if (acciones[i] == "saltar" && obstáculos[i] != '|') {
+                obstáculos[i] = 'x'
+                correcto = false
+            } else if (acciones[i] == "correr" && obstáculos[i] == '_') {
+            // Correcto
+            } else if (acciones[i] == "saltar" && obstáculos[i] == '|') {
+            // Correcto
+            } else {
+                obstáculos[i] = '?'
+            }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            // obstáculos.add('?')
+        }
+    }
+    pista = obstáculos.joinToString()
+    return correcto
+}
+
+fun pistaCorrecta(pista: String): Boolean {
+    val obstáculos = pista.toCharArray()
+    var contador = 0
+    obstáculos.forEach { obstáculo ->
+        when (obstáculo) {
+            '_' -> contador++
+            '|' -> contador++
+        }
+    }
+    return contador == obstáculos.size
+}
+
+fun accionesCorrectas(acciones: Array<String>): Boolean {
+    var contador = 0
+    acciones.forEach { acción ->
+        when (acción) {
+            "correr" -> contador++
+            "saltar" -> contador++
+        }
+    }
+    return contador == acciones.size
 }
