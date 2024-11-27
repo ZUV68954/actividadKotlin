eea9aef9d536629a44b6d4992d8df02923061ff7
ZUV68954 <ZUV68954@educastur.es>
2024-11-27 18:55:11 +0100

    Ejercicio 5 completado.

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,109 @@
+val tresEnRaya: ArrayList<ArrayList<String>> = arrayListOf(
+    ArrayList(), ArrayList(), ArrayList()
+)
+
 fun main() {
-    println("Hello World!")
+    val línea1: Array<String> = arrayOf("X", "O", "X")
+    val línea2: Array<String> = arrayOf("O", "X", "O")
+    val línea3: Array<String> = arrayOf("O", "O", "X")
+    crearPartida(línea1, línea2, línea3)
+    println(comprobarGanador())
+}
+
+
+fun crearPartida(línea0: Array<String>, línea1: Array<String>, línea2: Array<String>) {
+    línea0.forEach { símbolo ->
+        tresEnRaya[0].add(símbolo)
+    }
+    línea1.forEach { símbolo ->
+        tresEnRaya[1].add(símbolo)
+    }
+    línea2.forEach { símbolo ->
+        tresEnRaya[2].add(símbolo)
+    }
+}
+
+fun comprobarGanador(): String {
+    var totalX = 0
+    var totalO = 0
+
+    // Comprobar línea en horizontal
+    for (j in 0..<3) {
+        var x = 0
+        var o = 0
+        for (i in 0..<3) {
+            if (tresEnRaya[j][i] == "X") {
+                x++
+                totalX++
+            } else if (tresEnRaya[j][i] == "O") {
+                o++
+                totalO++
+            }
+        }
+
+        // Descargo de responsablidad: Este código fue creado en base a una fórmula preestablecida.
+        if (totalO >= totalX + 2 || totalX >= totalO + 2) {
+            return "NULL"
+        }
+
+        if (x == 3) {
+            return "X"
+        } else if (o == 3) {
+            return "O"
+        }
+    }
+
+
+    // Comprobar línea en vertical
+    for (j in 0..<3) {
+        var x = 0
+        var o = 0
+        for (i in 0..<3) {
+            if (tresEnRaya[i][j] == "X") {
+                x++
+            } else if (tresEnRaya[i][j] == "O") {
+                o++
+            }
+        }
+        if (x == 3) {
+            return "X"
+        } else if (o == 3) {
+            return "O"
+        }
+    }
+
+    // Comprobar línea en diagonal de izquierda a derecha
+    var x = 0
+    var o = 0
+    for ((j, i) in (0..<3).withIndex()) {
+        if (tresEnRaya[j][i] == "X") {
+            x++
+        } else if (tresEnRaya[j][i] == "O") {
+            o++
+        }
+        if (x == 3) {
+            return "X"
+        } else if (o == 3) {
+            return "O"
+        }
+    }
+
+    // Comprobar línea en diagonal de derecha a izquierda
+    x = 0
+    o = 0
+    for ((j, i) in (2 downTo 0).withIndex()) {
+        if (tresEnRaya[j][i] == "X") {
+            x++
+        } else if (tresEnRaya[j][i] == "O") {
+            o++
+        }
+        if (x == 3) {
+            return "X"
+        } else if (o == 3) {
+            return "O"
+        }
+    }
+
+    // Si no es nulo, ni ha ganado X, ni ha ganado O. Tenemos un empate.
+    return "EMPATE"
 }
